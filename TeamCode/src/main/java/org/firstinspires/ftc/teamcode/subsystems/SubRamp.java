e96fbb1185a8130bcf90f66ebca39bc3c8c96866
Gautam <gautamgoatsharma@gmail.com>
2026-07-20 15:10:02 -0700

    Multi Ball

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SubRamp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SubRamp.java
@@ -1,21 +1,36 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import java.util.Set;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.impl.ServoEx;
+import dev.nextftc.hardware.positionable.SetPosition;
 
 public class SubRamp implements Subsystem {
+
     public static final SubRamp INSTANCE = new SubRamp();
     private SubRamp(){}
+
     private ServoEx RightRamp = new ServoEx("RRamp");
     private ServoEx LeftRamp = new ServoEx("LRamp");
 
-    @Override
-    public void initialize() {
-        // initialization logic (runs on init)
+    public static final double UP_POS = 0.5;
+    public static final double DOWN_POS = 0.7;
+
+    public Command RampDown = new SetPosition(RightRamp,1).requires(this);
+
+    public Command setLeftRampPos(double pos) {
+        return new InstantCommand(() -> LeftRamp.setPosition(pos));
     }
+    @Override
+    public void initialize(){}
     @Override
     public void periodic(){
-        // periodic logic (runs every loop)
+        LeftRamp.setPosition(1- RightRamp.getPosition());
     }
+
+
 
 }
