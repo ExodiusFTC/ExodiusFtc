a497315ea5fa4e539e424607ece3e5b71956af6f
pmayusm <pmayusm@gmail.com>
2026-09-13 13:54:37 -0700

    pedrro 3.0 - started setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+
+
+import java.util.Set;
+
+public class Robot implements  {
+
+}
